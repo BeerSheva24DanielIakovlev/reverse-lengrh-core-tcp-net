91c267f5a4c205a50e02ef8d7c71ce5ddf857864
Daniel <gromdan90@gmail.com>
2024-11-06 20:26:42 +0200

    HW 38

--- reverse-length-client-tcp-net/src/main/java/telran/appl/net/Main.java
+++ reverse-length-client-tcp-net/src/main/java/telran/appl/net/Main.java
@@ -1,7 +1,36 @@
 package telran.appl.net;
 
+import telran.view.*;
+import telran.net.TcpClient;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        InputOutput io = new StandardInputOutput();
+
+        String host = io.readString("Enter hostname:");
+        int port = io.readInt("Enter port:", "Invalid port");
+
+        try (TcpClient client = new TcpClient(host, port)) {
+            Menu menu = new Menu("Client Menu", getMenuItems(client));
+            menu.perform(io);
+        } catch (Exception e) {
+            io.writeLine("Connection error: " + e.getMessage());
+        }
+    }
+
+    private static Item[] getMenuItems(TcpClient client) {
+        return new Item[]{
+            Item.of("Reverse String", io -> {
+                String input = io.readString("Enter any string:");
+                String response = client.sendAndReceive("reverse", input);
+                io.writeLine("Response from server: " + response);
+            }),
+            Item.of("Get Length", io -> {
+                String input = io.readString("Enter any string:");
+                String response = client.sendAndReceive("length", input);
+                io.writeLine("Response from server: " + response);
+            }),
+            Item.ofExit()
+        };
     }
 }
--- reverse-length-server-tcp-net/src/main/java/telran/appl/net/Main.java
+++ reverse-length-server-tcp-net/src/main/java/telran/appl/net/Main.java
@@ -1,7 +1,11 @@
 package telran.appl.net;
 
+import telran.net.TcpServer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ReverseLengthProtocol protocol = new ReverseLengthProtocol();
+        TcpServer server = new TcpServer(protocol, 6000);
+        server.run();
     }
 }
